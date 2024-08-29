3a2d8c208650df0dd6e66f2295da9e4d437be2c3
shunechkabon <bondaruk.aleksandra92@gmail.com>
2024-08-29 23:52:11 +0100

    add generics

---
+++ goit-typescript-hw-01/src/generics/4.ts
@@ -0,0 +1,17 @@
+// Завдання 4
+
+type User = {
+    name: string;
+    surname: string;
+    email: string;
+    password: string;
+}
+
+function createOrUpdateUser(initialValues: Partial<User>) {
+    // Оновлення користувача
+}
+
+createOrUpdateUser({ 
+    email: 'user@mail.com',
+    password: 'password123' 
+});
---
+++ goit-typescript-hw-01/src/generics/5.ts
@@ -0,0 +1,14 @@
+// Завдання 5
+
+export enum UserRole {
+    admin = 'admin',
+    editor = 'editor',
+    guest = 'guest',
+}
+
+// Замініть наступний код на версію за допомогою Record
+const RoleDescription: Record<UserRole, string> = {
+    [UserRole.admin]: 'Admin User',
+    [UserRole.editor]: 'Editor User',
+    [UserRole.guest]: 'Guest User',
+};
